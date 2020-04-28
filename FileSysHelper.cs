b490d0ccc458173cb2bc0d5c001f456fcb2f1936
Hansong Li@SEG <hansong.li@seg-automotive.com>
2020-04-28 12:30:00 +0800

    Adding Functions

---
+++ FileSysHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.IO;
+using System;
+
+namespace SysEnvHelper
+{
+    public static class FileSysHelper
+    {
+        public static string GetHome(string relativePath)
+        {
+            string rootPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string path = rootPath + @"\" + relativePath;
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            return path;
+        }
+        public static void SaveFileToPath(string fileName, string path, string saveStr)
+        {
+            File.WriteAllText(path + @"\" + fileName, saveStr);
+        }
+        public static string GetCurrentAppLocationPath()
+        {
+            return Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        }
+
+    }
+}
---
+++ RandomSupporter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace SysEnvHelper
+{
+    public static class RandomSupporter
+    {
+        static Random random = new Random ();
+        static char[] constant = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        public static T RandomSelect<T> (this IEnumerable<T> enumerable)
+        {
+            return enumerable.ElementAt (random.Next (enumerable.Count ()));
+        }
+        public static char GetRandomChar ()
+        {
+            return constant.RandomSelect ();
+        }
+        public static char GetRandomLetter ()
+        {
+            return constant.ElementAt (random.Next (10, 36));
+        }
+
+    }
+}
