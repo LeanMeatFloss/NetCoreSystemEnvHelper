a214d772a9d8f46eebc8418a483b56f89b257257
Hansong Li@SEG <hansong.li@seg-automotive.com>
2020-04-29 11:06:01 +0800

    Reorder code

--- FileSysHelper.cs
+++ FileSysHelper.cs
@@ -1,28 +1,28 @@
-﻿using System.Reflection;
+﻿using System;
 using System.IO;
-using System;
+using System.Reflection;
 
-namespace SysEnvHelper
+namespace NetCoreSystemEnvHelper
 {
     public static class FileSysHelper
     {
-        public static string GetHome(string relativePath)
+        public static string GetHome (string relativePath)
         {
-            string rootPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string rootPath = Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments);
             string path = rootPath + @"\" + relativePath;
-            if (!Directory.Exists(path))
+            if (!Directory.Exists (path))
             {
-                Directory.CreateDirectory(path);
+                Directory.CreateDirectory (path);
             }
             return path;
         }
-        public static void SaveFileToPath(string fileName, string path, string saveStr)
+        public static void SaveFileToPath (string fileName, string path, string saveStr)
         {
-            File.WriteAllText(path + @"\" + fileName, saveStr);
+            File.WriteAllText (path + @"\" + fileName, saveStr);
         }
-        public static string GetCurrentAppLocationPath()
+        public static string GetCurrentAppLocationPath ()
         {
-            return Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            return Path.GetDirectoryName (Assembly.GetEntryAssembly ().Location);
         }
 
     }
